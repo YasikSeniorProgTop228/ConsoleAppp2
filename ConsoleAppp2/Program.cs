8c06b18a34ec8201e9a4a47bc2e86fa27ddaca3c
YasikSeniorProgTop228 <lugovijaroslav9@gmail.com>
2024-09-24 20:21:19 +0300

    Add project files.

---
+++ ConsoleAppp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Запит у користувача висоти трикутника
+        Console.Write("Введіть висоту трикутника: ");
+        int height;
+
+        // Перевірка на правильність введення
+        if (int.TryParse(Console.ReadLine(), out height) && height > 0)
+        {
+            for (int i = 1; i <= height; i++)
+            {
+                // Вивід пробілів для вирівнювання
+                Console.Write(new string(' ', height - i));
+
+                // Вивід цифр
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+
+                // Перехід на новий рядок
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Будь ласка, введіть коректну висоту трикутника.");
+        }
+    }
+}
+
